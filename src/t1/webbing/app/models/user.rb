b8d437cf1f3677f1a91bffc7198f231b75842e23
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2017-10-17 14:29:55 -0200

    Adding validations for user

--- src/t1/webbing/app/models/user.rb
+++ src/t1/webbing/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+	has_secure_password
+
+	EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :username, presence: true, length: {maximum: 80}
+	validates :password, presence: true, length: {minimum: 8}
+	validates :password_confirmation, length: { minimum: 8 }, presence: true
+	validates :email, presence: true, format: { with: EMAIL }, uniqueness: true
 end
