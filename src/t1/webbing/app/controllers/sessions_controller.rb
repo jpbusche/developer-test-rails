3af5a9989922f08648eda0173123c20b1c8bb183
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2017-10-17 15:24:10 -0200

    Adding sessions for users

---
+++ src/t1/webbing/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email])
+  	if user && user.authenticate(params[:session][:password])
+  		session[:user_id] = user.id
+  		redirect_to user_path(user.id)
+  	else
+  		redirect_to root_path
+  	end
+  end
+
+  def destroy
+  	if current_user
+  		session[:user_ir] = nil
+  	end
+  	redirect_to root_path
+  end
+
+end
--- src/t1/webbing/config/routes.rb
+++ src/t1/webbing/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+  
+  resources :users
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
