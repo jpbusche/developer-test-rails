6f802bc537bab4cb1ef90238ade265e5085ab90f
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2017-10-20 17:15:39 -0200

    Fixing some issues

--- src/t1/webbing/app/models/webpage.rb
+++ src/t1/webbing/app/models/webpage.rb
@@ -3,6 +3,7 @@ class Webpage < ApplicationRecord
 	has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
 
 	validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
+	validates :logo, presence: true
 	validates :title, presence: true, length: {maximum: 80}
 	validates :description, presence: true, length: {maximum: 240}
 	validates :body, presence: true
--- src/t2/webbing-api/db/seeds.rb
+++ src/t2/webbing-api/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor finibus metus et porta. Morbi ullamcorper lacus porttitor hendrerit vehicula. Nulla elementum ex massa, ut imperdiet nunc congue nec."
+@webpage1 = Webpage.create(title: "Google", description: "Site de pesquisa", body: text, author: "Larry Page", url: "www.google.com.br")
+@webpage2 = Webpage.create(title: "Facebook", description: "Rede Social", body: text, author: "Mark Zuckerberg", url: "facebook.com")
+@webpage3 = Webpage.create(title: "Orkut", description: "Rede Social", body: text, author: "Orkut Büyükkökten", url: "www.orkut.com.br")
+@webpage4 = Webpage.create(title: "Bing", description: "Site de pesquisa", body: text, author: "Steve Ballmer", url: "www.bing.com")
