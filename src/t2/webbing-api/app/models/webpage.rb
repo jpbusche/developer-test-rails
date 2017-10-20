27f8e7555534e9dff3f8b4da3ade2805636d2766
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2017-10-20 02:03:43 -0200

    Adding validations in webpages model

--- src/t2/webbing-api/app/models/webpage.rb
+++ src/t2/webbing-api/app/models/webpage.rb
@@ -1,2 +1,8 @@
 class Webpage < ApplicationRecord
+	
+	validates :title, presence: true, length: {maximum: 80}
+	validates :description, presence: true, length: {maximum: 240}
+	validates :body, presence: true
+	validates :url, presence: true, uniqueness: true
+	validates :author, presence: true, length: {maximum: 80}
 end
